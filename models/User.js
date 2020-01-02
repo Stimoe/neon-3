776b9fbe53ac294b2ff01a3c2858f6c2d2858e81
Stimoe <stimoe3@gmail.com>
2020-01-02 12:42:28 -0800

    working on bcryptjs

--- models/User.js
+++ models/User.js
@@ -1,25 +1,25 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// const {Schema} = mongoose;
+const {Schema} = mongoose;
 
-// const userSchema = new Schema({
-//     username: String,
-//     password: String,
-// })
+const userSchema = new Schema({
+    username: String,
+    password: String,
+})
 
-// mongoose.model('users', userSchema);
+mongoose.model('users', userSchema);
 
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-// Create Schema
-const UserSchema = new Schema({
-  username: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
-});
-module.exports = User = mongoose.model("users", UserSchema);
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+// // Create Schema
+// const UserSchema = new Schema({
+//   username: {
+//     type: String,
+//     required: true
+//   },
+//   password: {
+//     type: String,
+//     required: true
+//   }
+// });
+// module.exports = User = mongoose.model("users", UserSchema);
