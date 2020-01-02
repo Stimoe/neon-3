bced92e0f744533c9fe1b1773403dd93ae36697f
Stimoe <stimoe3@gmail.com>
2020-01-02 12:46:50 -0800

    working on bcryptjs

--- client/src/pages/auth/register.js
+++ client/src/pages/auth/register.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+const mongoose = require('mongoose');
+const User = mongoose.model('users');
+const Bcrypt = require("bcryptjs");
 import axios from "axios";
 import "./style.css";
 
@@ -81,9 +84,34 @@ class Register extends Component {
   handleLoginFormSubmit = event => {
 
     event.preventDefault();
-axios
-
-  }
+axios.post('/login', function (req, res) {
+      User.findOne({
+          where: {
+              username: req.body.username
+          }
+      }).then(function (User) {
+          if (!User) {
+              res.status(500).send("no such user")
+          }
+          else {
+  
+              //compares password send in req.body to one in database, will return true if matched.
+              if (Bcrypt.compareSync(req.body.password, User.password)) {
+                  //create new session property "user", set equal to logged in user
+                  req.session.user = { id: User.id, name: User.name }
+                  req.session.error = null;
+                  res.status(200).json(req.session);
+              }
+              else {
+                  //delete existing user, add error
+                  req.session.user = false;
+                  req.session.error = 'auth failed bro';
+                  res.status(401).send("password incorrect");
+              }
+          }
+      })
+  })
+}
 
 
 
