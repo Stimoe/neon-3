2e2b2c31313698bd2327173e2ce9595a8acb93a1
Stimoe <stimoe3@gmail.com>
2020-01-02 15:17:40 -0800

    working on bcryptjs

--- client/src/pages/battleArea/index.js
+++ client/src/pages/battleArea/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import Component from "react"
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./style.css";
 import enemies from "../../enemy.json";
