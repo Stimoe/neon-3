b383e7258a208897cb3566cac33555bb8eee2277
Stimoe <stimoe3@gmail.com>
2020-01-03 15:58:21 -0800

    working on bcryptjs

--- client/src/pages/auth/login.js
+++ client/src/pages/auth/login.js
@@ -43,6 +43,9 @@ class Login extends Component {
     handleLoginFormSubmit = event => {
       event.preventDefault();
       axios.post('/api/user/login', { username: this.state.username, password: this.state.password }).then(res => {
+        console.log(this.state.username);
+        console.log(this.state.password);
+        
         console.log("line 26 ", res.data, res.status)
         console.log(res.data.message);
         
