829c6ff2323194d4693adab22e56d5f6f01e0ade
Stimoe <stimoe3@gmail.com>
2020-01-03 15:46:45 -0800

    working on bcryptjs

--- models/User2.js
+++ models/User2.js
@@ -1,25 +1,25 @@
-// const Mongoose = require("mongoose");
-// const Bcrypt = require("bcryptjs");
+const Mongoose = require("mongoose");
+const Bcrypt = require("bcryptjs");
 
 
-// const UserSchema = new Mongoose.Schema({
-//     username: String,
-//     password: String
-// });
 
+const UserSchema = new Mongoose.Schema({
+    username: String,
+    password: String
+});
 
-// UserSchema.pre("save", function(next) {
-//     if(!this.isModified("password")) {
-//         return next();
-//     }
-//     this.password = Bcrypt.hashSync(this.password, 10);
-//     next();
-// });
+UserSchema.pre("save", function(next) {
+    if(!this.isModified("password")) {
+        return next();
+    }
+    this.password = Bcrypt.hashSync(this.password, 10);
+    next();
+});
 
-// UserSchema.methods.comparePassword = function(plaintext, callback) {
-//     return callback(null, Bcrypt.compareSync(plaintext, this.password));
-// };
+UserSchema.methods.comparePassword = function(plaintext, callback) {
+    return callback(null, Bcrypt.compareSync(plaintext, this.password));
+};
 
-// // const User = new Mongoose.model("user", UserSchema);
+const UserModel = new Mongoose.model("user", UserSchema);
 
-// module.exports = Mongoose.model('User', UserSchema);
+module.exports = Mongoose.model(UserModel);
--- routes/User.js
+++ routes/User.js
@@ -1,39 +1,35 @@
-// const mongoose = require('mongoose');
-// const User = mongoose.model('users');
-// const Bcrypt = require("bcryptjs");
-// var BCRYPT_SALT_ROUNDS = 12;
-// module.exports = (app) => {
 
-//   app.post("/api/user/register", async (request, response) => {
-//     try {
-//         var user = new User(request.body);
-//         var result = await user.save();
-//         response.send(result);
-//         response.redirect('/storypage');
-//     } catch (error) {
-//         response.status(500).send(error);
-//     }
-// });
+var mongoose = require('mongoose'),
+UserModel = require('../models/User2');
+Bcrypt = require("bcryptjs");
+module.exports = (app) => {
 
-// app.post("/api/user/login", async (request, response) => {
-//   try {
-//       var user = await User.findOne({ username: request.body.username }).exec();
-//       if(!user) {
-//           return response.status(400).send({ message: "The username does not exist" }, response.redirect('/register') );
-    
-//       }
-//       user.comparePassword(request.body.password, (error, match) => {
-//           if(!match) {
-//               return response.status(400).send({ message: "The password is invalid" }, response.redirect('/login'));
-//           }
-//       });
-//       response.send({ message: "The username and password combination is correct!" }, response.redirect('/storypage'));
-//   } catch (error) {
-//       response.status(500).send(error);
-//   }
-// });
+    app.post("/api/user/register", async (request, response) => {
+        try {
+            var user = new UserModel(request.body);
+            var result = await user.save();
+            response.send(result);
+        } catch (error) {
+            response.status(500).send(error);
+        }
+    });
+
+    app.post("/api/user/login", async (request, response) => {
+        try {
+            var user = await UserModel.findOne({ username: request.body.username }).exec();
+            if(!user) {
+                return response.status(400).send({ message: "The username does not exist" });
+            }
+            user.comparePassword(request.body.password, (error, match) => {
+                if(!match) {
+                    return response.status(400).send({ message: "The password is invalid" });
+                }
+            });
+            response.send({ message: "The username and password combination is correct!" });
+        } catch (error) {
+            response.status(500).send(error);
+        }
+    });
 
 
-// }
-
-
+}
