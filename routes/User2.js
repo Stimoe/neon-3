296ab6f15de4ea3ffa55fc8f40fd19558bf5ee4f
Stimoe <stimoe3@gmail.com>
2020-01-03 13:56:54 -0800

    working on bcryptjs

--- routes/User2.js
+++ routes/User2.js
@@ -10,7 +10,7 @@ app.post("/api/user/register", async (request, response) => {
         request.body.password = Bcrypt.hashSync(request.body.password, 10);
         var user = new User(request.body);
         var result = await user.save();
-        response.send(result)
+        return response.status(200).send(result)
      
     } catch (error) {
         response.status(500).send(error);
@@ -21,10 +21,10 @@ app.post("/api/user/login", async (request, response) => {
     try {
         var user = await User.findOne({ username: request.body.username }).exec();
         if(!user) {
-            return response.status(400).send({ message: "The username does not exist" }, response.redirect('/register'));
+            return response.status(400).send({ message: "The username does not exist" });
         }
         if(!Bcrypt.compareSync(request.body.password, user.password)) {
-            return response.status(400).send({ message: "The password is invalid" }, response.redirect('/login'));
+            return response.status(400).send({ message: "The password is invalid" });
         }
         response.send({ message: "The username and password combination is correct!" });
     } catch (error) {
