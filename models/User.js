bc4308b0251c7be2fd332bedb6e6532d16b0e5f4
Stimoe <stimoe3@gmail.com>
2020-01-03 11:25:18 -0800

    working on bcryptjs

--- models/User.js
+++ models/User.js
@@ -1,25 +1,41 @@
-const mongoose = require('mongoose');
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    bcrypt = require('bcrypt'),
+    SALT_WORK_FACTOR = 10;
 
-const {Schema} = mongoose;
+var User = new Schema({
+    username: { type: String, required: true, index: { unique: true } },
+    password: { type: String, required: true }
+});
 
-const userSchema = new Schema({
-    username: String,
-    password: String,
-})
 
-mongoose.model('users', userSchema);
+User.pre('save', function(next) {
+    var user = this;
 
-// const mongoose = require("mongoose");
-// const Schema = mongoose.Schema;
-// // Create Schema
-// const UserSchema = new Schema({
-//   username: {
-//     type: String,
-//     required: true
-//   },
-//   password: {
-//     type: String,
-//     required: true
-//   }
-// });
-// module.exports = User = mongoose.model("users", UserSchema);
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) return next(err);
+
+        // hash the password using our new salt
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+User.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
+
+module.exports = mongoose.model('User', User);
+
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,81 +1,37 @@
-const mongoose = require('mongoose');
-const User = mongoose.model('users');
-const Bcrypt = require("bcryptjs");
-var BCRYPT_SALT_ROUNDS = 12;
-module.exports = (app) => {
+var db = require('../models/User');
+var bcrypt = require('bcrypt');
+const saltRounds = 10;
 
-  app.get(`/api/user`, async (req, res) => {
-    let users = await User.find();
-    return res.status(200).send(users);
+app.post('/api/user/register', function (req, res) {
+    bcrypt.hash(req.body.password, saltRounds, function (err,   hash) {
+   db.User.create({
+     name: req.body.username,
+     password: hash
+     }).then(function(data) {
+      if (data) {
+      res.redirect('/login');
+      }
+    });
+   });
   });
 
-
-
-  app.post("/api/user/register", async (request, response) => {
-    try {
-        request.body.password = Bcrypt.hashSync(request.body.password, 10);
-        var user = new User(request.body);
-        var result = await user.save();
-        response.send(result);
-    } catch (error) {
-        response.status(500).send(error);
+  app.post('/api/user/login', function (req, res) {
+    db.User.findOne({
+         where: {
+             username: req.body.username
+                }
+    }).then(function (user) {
+        if (!user) {
+           res.redirect('/register');
+        } else {
+bcrypt.compare(req.body.password, user.password, function (err, result) {
+       if (result == true) {
+           res.redirect('/storypage');
+       } else {
+        res.send('Incorrect password');
+        res.redirect('/login');
+       }
+     });
     }
+ });
 });
-
-
-
-app.post("/api/user/login", async (request, response) => {
-  try {
-      var user = await User.findOne({ username: request.body.username }).exec();
-      if(!user) {
-          return response.status(400).send({ message: "The username does not exist" });
-      }
-      if(!Bcrypt.compareSync(request.body.password, user.password)) {
-          return response.status(400).send({ message: "The password is invalid" });
-      }
-      response.send({ message: "The username and password combination is correct!" });
-  } catch (error) {
-      response.status(500).send(error);
-  }
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  app.put(`/api/user/:id`, async (req, res) => {
-    const {id} = req.params;
-
-    let user = await User.findByIdAndUpdate(id, req.body);
-
-    return res.status(202).send({
-      error: false,
-      user
-    })
-
-  });
-
-  app.delete(`/api/user/:id`, async (req, res) => {
-    const {id} = req.params;
-
-    let user = await User.findByIdAndDelete(id);
-
-    return res.status(202).send({
-      error: false,
-      user
-    })
-
-  })
-
-}
