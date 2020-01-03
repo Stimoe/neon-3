79aa5f9f8092bc4974bcb57a5c7c7c9f7578fd03
Stimoe <stimoe3@gmail.com>
2020-01-03 14:00:13 -0800

    working on bcryptjs

--- client/src/pages/auth/register.js
+++ client/src/pages/auth/register.js
@@ -51,15 +51,15 @@ class Register extends Component {
 
     axios.post('/api/user/register', { username: this.state.username, password: this.state.password }).then(res => {
       console.log("line 26 ", res.status)
-      let newStatus= res.status
+      let newStatus= (res.status)
       console.log("newStatus ",newStatus);
       
 
-    // if(newStatus===200){
-    //     this.setState({ redirect: true })
-    //     console.log(this.state.redirect)
-    // }
-    // this.props.history.push("/storypage")
+    if(newStatus===200){
+        // this.setState({ redirect: true })
+        console.log(this.state.redirect)
+    }
+    
     
     }).catch(err => {
       console.log(err.response);
