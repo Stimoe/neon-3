9227141e882c818c233ce9d82dd07e72a63e61d2
Stimoe <stimoe3@gmail.com>
2020-01-04 14:01:44 -0800

    more bcrypt

--- models/User2.js
+++ models/User2.js
@@ -1,25 +1,39 @@
-const Mongoose = require("mongoose");
-const Bcrypt = require("bcryptjs");
+// var mongoose = require('mongoose'),
+//     Schema = mongoose.Schema,
+//     bcrypt = require('bcryptjs'),
+//     SALT_WORK_FACTOR = 10;
 
+// var UserSchema = new Schema({
+//     username: { type: String, required: true, index: { unique: true } },
+//     password: { type: String, required: true }
+// });
 
+// UserSchema.pre('save', function(next) {
+//     var user = this;
 
-const UserSchema = new Mongoose.Schema({
-    username: String,
-    password: String
-});
+//     // only hash the password if it has been modified (or is new)
+//     if (!user.isModified('password')) return next();
 
-UserSchema.pre("save", function(next) {
-    if(!this.isModified("password")) {
-        return next();
-    }
-    this.password = Bcrypt.hashSync(this.password, 10);
-    next();
-});
+//     // generate a salt
+//     bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+//         if (err) return next(err);
 
-UserSchema.methods.comparePassword = function(plaintext, callback) {
-    return callback(null, Bcrypt.compareSync(plaintext, this.password));
-};
+//         // hash the password using our new salt
+//         bcrypt.hash(user.password, salt, function(err, hash) {
+//             if (err) return next(err);
 
+//             // override the cleartext password with the hashed one
+//             user.password = hash;
+//             next();
+//         });
+//     });
+// });
 
+// UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+//     bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+//         if (err) return cb(err);
+//         cb(null, isMatch);
+//     });
+// };
 
-module.exports = Mongoose.model("user", UserSchema);
+// module.exports = mongoose.model('User', UserSchema);
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,50 @@
-// const express = require('express');
-// const router = express.Router();
-// const userController = require('../controllers/users');
-// router.post('/register', userController.create);
-// router.post('/authenticate', userController.authenticate);
-// module.exports = router;
+
+const bcrypt = require("bcrypt");
+
+const userSchema = require("../models/User");
+
+module.exports = (app) => {
+// Sign-up
+app.post("/api/user/register", (req, res, next) => {
+    bcrypt.hash(req.body.password, 10).then((hash) => {
+        const user = new userSchema({
+            username: req.body.username,
+            password: hash
+        });
+        user.save().then((response) => {
+            res.status(201).json({
+                message: "User successfully created!",
+                result: response
+            });
+        }).catch(error => {
+            res.status(500).json({
+                error: error
+            });
+        });
+    });
+});
+
+app.post("/api/user/login", (req, res, next) => {
+  let getUser;
+  userSchema.findOne({
+      username: req.body.username
+  }).then(user => {
+      if (!user) {
+          return res.status(401).json({
+              message: "Authentication failed"
+          });
+      }
+      return bcrypt.compare(req.body.password, user.password);
+  }).then(response => {
+      if (!response) {
+          return res.status(401).json({
+              message: "Authentication failed"
+          });
+      }
+  }).catch(err => {
+      return res.status(401).json({
+          message: "Authentication failed"
+      });
+  });
+});
+}
