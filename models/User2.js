baef9feaa60aa91d99264510051a6a42dee00d92
Stimoe <stimoe3@gmail.com>
2020-01-05 12:09:16 -0800

    more bcrypt

--- models/User2.js
+++ models/User2.js
@@ -10,11 +10,9 @@ var userSchema = mongoose.Schema({
 
 
 
-var User = mongoose.model('User', userSchema);
 
-module.exports = {
-  User: User
-};
+
+
 
 module.exports.createUser = function(newUser, callback){
     bcrypt.genSalt(10, function(err, salt) {
@@ -36,3 +34,7 @@ module.exports.comparePassword = function(candidatePassword, hash, callback){
         callback(null, isMatch);
     });
 }
+
+
+var User = mongoose.model('User', UserSchema);
+module.exports = User;
--- routes/User.js
+++ routes/User.js
@@ -1,49 +1,35 @@
-var bcrypt = require('bcryptjs');
+
 var User = require('../models/User2');
 
 
-const saltRounds = 10
 module.exports = (app) => {
 
     app.post('/api/user/register', function (req, res, next) {
-        var username = req.body.username;
-        var password = req.body.password;
+        User.createUser(req.body, function (err, user) {
+            if (err) throw err;
+            return done(null, false, { message: 'Made new user' });
 
-      User.createUser(req.body, function(err, user){
-        if(err) throw err;
-        return done(null, false, {message: 'Made new user'});
-        
-        
-        
-      })
-
+        })
     })
 
     app.post('/api/user/login', function (req, res, next) {
         let username = req.body.username;
         let password = req.body.password;
-       
-            User.getUserByUsername(username, function(err, user){
-              if(err) throw err;
-              if(!user){
-                return done(null, false, {message: 'Unknown User'});
+
+        User.getUserByUsername(username, function (err, user) {
+            if (err) throw err;
+            if (!user) {
+                return done(null, false, { message: 'Unknown User' });
             }
-        
-            User.comparePassword(password, user.password, function(err, isMatch){
-              if(err) throw err;
-              if(isMatch){
-                return done(null, user);
-              } else {
-                return done(null, false, {message: 'Invalid password'});
-              }
+
+            User.comparePassword(password, user.password, function (err, isMatch) {
+                if (err) throw err;
+                if (isMatch) {
+                    return done(null, user);
+                } else {
+                    return done(null, false, { message: 'Invalid password' });
+                }
             });
-           });
-
-
-
+        });
     })
-
-
-
-
 }
