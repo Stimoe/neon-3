79a883e1424c09278fd4cfb053cbd2567a99a63c
Stimoe <stimoe3@gmail.com>
2020-01-05 12:35:35 -0800

    more bcrypt

--- models/User2.js
+++ models/User2.js
@@ -4,16 +4,16 @@ const bcrypt = require('bcryptjs');
 //Define a schema
 const Schema = mongoose.Schema;
 const userSchema = new Schema({
- username: {
-  type: String,
-  trim: true,  
-  required: true,
- },
- password: {
-  type: String,
-  trim: true,
-  required: true
- }
+    username: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    password: {
+        type: String,
+        trim: true,
+        required: true
+    }
 });
 
 
@@ -21,31 +21,32 @@ const userSchema = new Schema({
 
 
 
-userSchema.methods.createUser = function(newUser, callback){
-        bcrypt.genSalt(10, function(err, salt) {
-            bcrypt.hash(newUser.password, salt, function(err, hash) {
-                newUser.password = hash;
-                newUser.save(callback);
-            });
+userSchema.methods.createUser = function (newUser, callback) {
+    bcrypt.genSalt(10, function (err, salt) {
+        bcrypt.hash(newUser.password, salt, function (err, hash) {
+            newUser.password = hash;
+            userSchema.save(callback);
         });
-    }
-    userSchema.methods.getUserByUsername = function(username, callback){
-        var query = {username: username};
-        User.findOne(query, callback);
-    }
-  
-    userSchema.methods.comparePassword = function(candidatePassword, hash, callback){
-        bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
-            if(err) throw err;
-            callback(null, isMatch);
-        });
-    }
+    });
+}
+userSchema.methods.getUserByUsername = function (username, callback) {
+    var query = { username: username };
+    User.findOne(query, callback);
+}
+
+userSchema.methods.comparePassword = function (candidatePassword, hash, callback) {
+    bcrypt.compare(candidatePassword, hash, function (err, isMatch) {
+        if (err) throw err;
+        callback(null, isMatch);
+    });
+}
+
+
+mongoose.model('User', userSchema);
+module.exports = mongoose.model('User');
 
 
 
-
-
-module.exports = mongoose.model('User', userSchema);
 
 
 
