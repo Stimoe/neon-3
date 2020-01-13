d5e0b6019723325d6055c905d29c1f9a4324ca0d
Stimoe <stimoe3@gmail.com>
2020-01-13 13:05:53 -0800

    testing modal

--- client/src/components/enemiesActionModul/index.jsx
+++ client/src/components/enemiesActionModul/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState, Component } from "react";
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
-
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar'
 
 
 
