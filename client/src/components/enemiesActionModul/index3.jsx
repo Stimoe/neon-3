11259c82540750ddc86f8e86d20d2ded4b6ed716
Stimoe <stimoe3@gmail.com>
2020-01-13 13:23:55 -0800

    testing modal

---
+++ client/src/components/enemiesActionModul/index3.jsx
@@ -0,0 +1,38 @@
+import React, { useState, Component } from "react";
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar'
+
+
+
+
+function EnemyModal(props) {
+    const [smShow, setSmShow] = useState(false);
+
+  
+    return (
+      <ButtonToolbar>
+        <Button onClick={() => setSmShow(true)}>Small modal</Button>
+ 
+  
+        <Modal
+          size="sm"
+          show={smShow}
+          onHide={() => setSmShow(false)}
+          aria-labelledby="example-modal-sizes-title-sm"
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="example-modal-sizes-title-sm">
+            Enemy Action
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>...</Modal.Body>
+        </Modal>
+  
+  
+      </ButtonToolbar>
+    );
+  }
+  
+  export default EnemyModal;
+
