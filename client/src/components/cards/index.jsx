e0f07729bd0b2970e6a59e82dabfa35b1de17860
Stimoe <stimoe3@gmail.com>
2020-07-22 16:17:52 -0700

    testing

--- client/src/components/cards/index.jsx
+++ client/src/components/cards/index.jsx
@@ -12,11 +12,12 @@ function Cards(props) {
       className={`card grow raise col-md-${props.colSize}`}
     >
       <div className="card-body flip-card-inner nes-pointer">
-        <div className="flip-card-front background">
-        </div>
+        <div className="flip-card-front background"></div>
         <div className="flip-card-back">
-          <h5 className="card-title">{props.name}</h5>
-          <p className="card-text">{props.text}</p>
+          <div className="inner-card">
+            <h5 className="card-title">{props.name}</h5>
+            <p className="card-text">{props.text}</p>
+          </div>
         </div>
       </div>
     </div>
